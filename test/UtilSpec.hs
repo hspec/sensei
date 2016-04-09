bb2291ccf33bb4693ff5cee0738281afa4b9ba08
Amar <amar@capital-match.com>
2016-04-09 14:52:24 +0800

    remove redundant do's in UtilSpec

--- test/UtilSpec.hs
+++ test/UtilSpec.hs
@@ -11,57 +11,57 @@ import           Util
 spec :: Spec
 spec = do
   describe "isHaskell" $ do
-    it "accepts hs files" $ do
+    it "accepts hs files" $
       isHaskell "/foo/bar/.git/baz/foo.hs" `shouldBe` True
 
-    it "accepts hs files" $ do
+    it "accepts hs files" $
       isHaskell "/foo/bar/.git/baz/foo.lhs" `shouldBe` True
 
   describe "isBoring" $ do
-    it "ignores files in .git/" $ do
+    it "ignores files in .git/" $
       isBoring "/foo/bar/.git/baz/foo.txt" `shouldBe` True
 
-    it "ignores files in dist/" $ do
+    it "ignores files in dist/" $
       isBoring "/foo/bar/dist/baz/foo.txt" `shouldBe` True
 
   describe "normalizeTypeSignatures" $ do
-    it "removes newlines from type signatures" $ do
+    it "removes newlines from type signatures" $
       normalizeTypeSignatures "foo\n  :: Int" `shouldBe` "foo :: Int"
 
-    it "replaces unicode characters" $ do
+    it "replaces unicode characters" $
       normalizeTypeSignatures "head ∷ [a] → a" `shouldBe` "head :: [a] -> a"
 
-  describe "dotGhciWritableByOthers" $ do
+  describe "dotGhciWritableByOthers" $
     around_ inTempDirectory $ do
       before_ (touch ".ghci" >> callCommand "chmod go-w .ghci") $ do
-        it "returns False" $ do
+        it "returns False" $
           dotGhciWritableByOthers `shouldReturn` False
 
-        context "when writable by group" $ do
+        context "when writable by group" $
           it "returns True" $ do
             callCommand "chmod g+w .ghci"
             dotGhciWritableByOthers `shouldReturn` True
 
-        context "when writable by others" $ do
+        context "when writable by others" $
           it "returns True" $ do
             callCommand "chmod o+w .ghci"
             dotGhciWritableByOthers `shouldReturn` True
 
-        context "when directory is writable by group" $ do
+        context "when directory is writable by group" $
           it "returns True" $ do
             callCommand "chmod g+w ."
             dotGhciWritableByOthers `shouldReturn` True
 
-      context "when .ghci does not exist" $ do
-        it "returns False" $ do
+      context "when .ghci does not exist" $
+        it "returns False" $
           dotGhciWritableByOthers `shouldReturn` False
 
   describe "writableByOthers" $ do
-    it "returns False for owner" $ do
+    it "returns False for owner" $
       writableByOthers ownerWriteMode `shouldBe` False
 
-    it "returns True for group" $ do
+    it "returns True for group" $
       writableByOthers groupWriteMode `shouldBe` True
 
-    it "returns True for others" $ do
+    it "returns True for others" $
       writableByOthers otherWriteMode `shouldBe` True
