dca052b3101f4a4a2e87dad3f3f1f7405112382c
Simon Hengel <sol@typeful.net>
2024-09-08 07:28:56 +0700

    Refactoring

--- src/Client.hs
+++ src/Client.hs
@@ -2,34 +2,36 @@ module Client (client) where
 
 import           Imports
 
-import           Network.HTTP.Client
-import           Network.HTTP.Client.Internal (Response(..))
+import           Network.Socket
 import           Network.HTTP.Types
-import           Network.Socket (connect)
+import           Network.HTTP.Client
+import           Network.HTTP.Client.Internal (Connection, Response(..))
 import qualified Data.ByteString.Lazy as L
 
-import           HTTP (newSocket, socketAddr, socketName)
+import           HTTP (newSocket, socketName)
 
 client :: FilePath -> IO (Bool, L.ByteString)
-client dir = fromRight connectError <$> tryJust p go
+client dir = handleSocketFileDoesNotExist name $ do
+  manager <- newManager defaultManagerSettings {managerRawConnection = return newConnection}
+  Response{..} <- httpLbs "http://localhost/" manager
+  return (statusIsSuccessful responseStatus, responseBody)
   where
-    connectError :: (Bool, L.ByteString)
-    connectError = (False, "could not connect to " <> fromString (socketName dir) <> "\n")
+    name :: FilePath
+    name = socketName dir
 
-    p :: HttpException -> Maybe ()
-    p e = case e of
-      HttpExceptionRequest _ (ConnectionFailure se) -> guard (isDoesNotExistException se) >> Just ()
+    newConnection :: Maybe HostAddress -> String -> Int -> IO Connection
+    newConnection _ _ _ = do
+      sock <- newSocket
+      connect sock (SockAddrUnix name)
+      socketConnection sock 8192
+
+handleSocketFileDoesNotExist :: String -> IO (Bool, L.ByteString) -> IO (Bool, L.ByteString)
+handleSocketFileDoesNotExist name = fmap (either id id) . tryJust doesNotExist
+  where
+    doesNotExist :: HttpException -> Maybe (Bool, L.ByteString)
+    doesNotExist = \ case
+      HttpExceptionRequest _ (ConnectionFailure e) | isDoesNotExistException e -> Just (False, "could not connect to " <> fromString name <> "\n")
       _ -> Nothing
 
     isDoesNotExistException :: SomeException -> Bool
     isDoesNotExistException = maybe False isDoesNotExistError . fromException
-
-    go = do
-      manager <- newManager defaultManagerSettings {managerRawConnection = return newConnection}
-      Response{..} <- httpLbs "http://localhost/" manager
-      return (statusIsSuccessful responseStatus, responseBody)
-
-    newConnection _ _ _ = do
-      sock <- newSocket
-      connect sock $ socketAddr dir
-      socketConnection sock 8192
