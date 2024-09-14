17178341148211c632cf25c04f297be1ca97b02b
Simon Hengel <sol@typeful.net>
2024-09-14 16:37:30 +0700

    seito: Add --vim-config

--- driver/seito.hs
+++ driver/seito.hs
@@ -7,10 +7,12 @@ import           System.Environment
 import           Control.Monad
 import qualified Data.ByteString.Lazy as L
 
+import           Paths_sensei (getDataFileName)
+
 import           Client
 
 main :: IO ()
 main = do
-  (success, output) <- getArgs >>= client ""
+  (success, output) <- getArgs >>= client getDataFileName ""
   L.putStr output
   unless success exitFailure
