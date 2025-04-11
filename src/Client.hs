d04ee119078d1a1d9b434074216848284b6fc751
Simon Hengel <sol@typeful.net>
2025-04-11 17:31:34 +0700

    Add `seito trigger`

--- src/Client.hs
+++ src/Client.hs
@@ -6,14 +6,17 @@ import System.IO
 import Network.HTTP.Client
 
 import HTTP.Util (makeRequest)
+import Sensei.API qualified as API
 
 client :: FilePath -> [String] -> IO (Bool, LazyByteString)
 client dir args = case args of
   [] -> hIsTerminalDevice stdout >>= run
   ["--no-color"] -> run False
   ["--color"] -> run True
+  ["trigger"] -> API.trigger dir
   _ -> do
     hPutStrLn stderr $ "Usage: seito [ --color | --no-color ]"
+    hPutStrLn stderr $ "       seito trigger"
     return (False, "")
   where
     run :: Bool -> IO (Bool, LazyByteString)
